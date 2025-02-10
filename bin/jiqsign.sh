a79992bbff804da5894aebf2680e74a5fac74b7e
StevenReiss <spr@cs.brown.edu>
2025-02-10 18:33:13 -0500

    Need touse jakarta libraries directly.

--- bin/jiqsign.sh
+++ bin/jiqsign.sh
@@ -5,7 +5,17 @@ pushd $SRD > /dev/null
 SRD1=`pwd`
 popd > /dev/null
 
-java -jar $SRD1/iqsign.jar -server -L jiqsign.log -S -LD
+LIB=$SRD/../../ivy/lib
+echo check $LIB
+if test -d $LIB; then
+   echo use local lib
+else
+   LIB=/pro/ivy/lib
+fi
+
+echo java -cp $LIB/jakarta.mail.jar:$LIB/jakarta.activation.jar -jar $SRD1/iqsign.jar -server -L jiqsign.log -S -LD
+
+# java -jar $SRD1/iqsign.jar -server -L jiqsign.log -S -LD
 
 
 
