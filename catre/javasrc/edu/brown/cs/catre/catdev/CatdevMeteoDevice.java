bdd3234a5a76d555a370eba2e7b986da63d11b52
StevenReiss <spr@cs.brown.edu>
2025-04-14 13:50:18 -0400

    Add open-meteo based weather device

---
+++ catre/javasrc/edu/brown/cs/catre/catdev/CatdevMeteoDevice.java
@@ -0,0 +1,347 @@
+/********************************************************************************/
+/*										*/
+/*		CatdevMeteoDevice.java						*/
+/*										*/
+/*	Weather device using Open-Meteo 					*/
+/*										*/
+/********************************************************************************/
+/*	Copyright 2023 Brown University -- Steven P. Reiss		      */
+/*********************************************************************************
+ *  Copyright 2023, Brown University, Providence, RI.				 *
+ *										 *
+ *			  All Rights Reserved					 *
+ *										 *
+ *  Permission to use, copy, modify, and distribute this software and its	 *
+ *  documentation for any purpose other than its incorporation into a		 *
+ *  commercial product is hereby granted without fee, provided that the 	 *
+ *  above copyright notice appear in all copies and that both that		 *
+ *  copyright notice and this permission notice appear in supporting		 *
+ *  documentation, and that the name of Brown University not be used in 	 *
+ *  advertising or publicity pertaining to distribution of the software 	 *
+ *  without specific, written prior permission. 				 *
+ *										 *
+ *  BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS		 *
+ *  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND		 *
+ *  FITNESS FOR ANY PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY	 *
+ *  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY 	 *
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,		 *
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS		 *
+ *  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE 	 *
+ *  OF THIS SOFTWARE.								 *
+ *										 *
+ ********************************************************************************/
+
+
+
+package edu.brown.cs.catre.catdev;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import edu.brown.cs.catre.catre.CatreDevice;
+import edu.brown.cs.catre.catre.CatreLog;
+import edu.brown.cs.catre.catre.CatreParameter;
+import edu.brown.cs.catre.catre.CatreParameterRef;
+import edu.brown.cs.catre.catre.CatreProgramListener;
+import edu.brown.cs.catre.catre.CatreStore;
+import edu.brown.cs.catre.catre.CatreUniverse;
+import edu.brown.cs.ivy.file.IvyFile;
+
+class CatdevMeteoDevice extends CatdevDevice implements CatreProgramListener
+{
+
+
+/********************************************************************************/
+/*										*/
+/*	Private Storage 							*/
+/*										*/
+/********************************************************************************/
+
+private String	location_name;
+private double	location_latitude;
+private double	location_longitude;
+private String	unit_type;
+private boolean is_used;
+
+private static Collection<CatdevMeteoDevice> known_locations = null;
+private static TimerTask timer_task = null;
+
+private static final long POLL_RATE = 1000*60*10;		// every 10 minutes
+
+private static String METEO_URL = "https://api.open-meteo.com/v1/forecast?" +
+      "latitude=$(LAT)&longitude=$(LONG)&current=weather_code,temperature_2m";
+
+
+private static final Map<Integer,String> CONDITIONS;
+
+static {
+   CONDITIONS = new HashMap<>();
+   CONDITIONS.put(0,"Clear");
+   CONDITIONS.put(1,"Partly Cloudy");
+   CONDITIONS.put(2,"Partly Cloudy");
+   CONDITIONS.put(3,"Cloudy");
+   CONDITIONS.put(45,"Fog");
+   CONDITIONS.put(48,"Fog");
+   CONDITIONS.put(51,"Drizzle");
+   CONDITIONS.put(53,"Drizzle");
+   CONDITIONS.put(55,"Drizzle");
+   CONDITIONS.put(61,"Rain");
+   CONDITIONS.put(63,"Rain");
+   CONDITIONS.put(65,"Rain");
+   CONDITIONS.put(71,"Snow");
+   CONDITIONS.put(73,"Snow");
+   CONDITIONS.put(75,"Snow");
+   CONDITIONS.put(77,"Snow");
+   CONDITIONS.put(80,"Rain Showers");
+   CONDITIONS.put(81,"Rain Showers");
+   CONDITIONS.put(82,"Rain Showers");
+   CONDITIONS.put(85,"Snow Showers");
+   CONDITIONS.put(86,"Snow Showers");
+   CONDITIONS.put(95,"Thunderstorm");
+   CONDITIONS.put(96,"Thunderstorm");
+   CONDITIONS.put(99,"Thunderstorm");
+}
+
+
+/********************************************************************************/
+/*										*/
+/*	Constructors								*/
+/*										*/
+/********************************************************************************/
+
+CatdevMeteoDevice(CatreUniverse cu,CatreStore cs,Map<String,Object> map)
+{
+   super(cu);
+   location_name = null;
+   location_latitude = 0;
+   location_longitude = 0;
+   unit_type = "imperial";
+   is_used = true;
+
+   fromJson(cs,map);
+
+   if (cu.getProgram() != null){
+      cu.getProgram().addProgramListener(this);
+    }
+
+   initialize();
+}
+
+
+private void initialize()
+{
+   String did = "OpenMeteo_" + location_latitude + "_" + location_longitude +
+      "_" + unit_type + "_" + location_name;
+   setDeviceId(did);
+
+   if (getName() == null) {
+      String nm = location_name;
+      if (nm == null || nm.isEmpty()) {
+	 nm = location_latitude + "," + location_longitude;
+       }
+      setName("Weather-" + nm);
+      setLabel("Weather for " + nm);
+    }
+
+   CatreParameter pp = for_universe.createRealParameter("Temperature",
+	 -100,160);
+   pp.setIsSensor(true);
+   addParameter(pp);
+   CatreParameter pp1 = for_universe.createStringParameter("WeatherCondition");
+   pp1.setIsSensor(true);
+   addParameter(pp1);
+   // might want to add other weather conditions
+   //		humidity, wind speed and direction
+
+   addMeteoDevice(this);
+}
+
+@Override public boolean validateDevice()
+{
+   if (location_latitude == 0 && location_longitude == 0) return false;
+   if (unit_type == null) return false;
+
+   return super.validateDevice();
+}
+
+
+
+@Override public void updateParameterValues()
+{
+   List<CatdevMeteoDevice> devs = List.of(this);
+   updateData(devs);
+}
+
+
+
+private void update(JSONObject data)
+{
+   double lat = data.getDouble("latitude");
+   double lng = data.getDouble("longitude");
+   if (lat != location_latitude || lng != location_longitude) {
+      CatreLog.logD("CATDEV","Lat/Long don't match " + lat + " " + lng + " " +
+            location_latitude + " " + location_longitude);
+    }
+   JSONObject units = data.getJSONObject("current_units");
+   JSONObject current = data.getJSONObject("current");
+   int code = current.getInt("weather_code");
+   String cond = CONDITIONS.get(code);
+   if (cond == null) cond = "Unknown";
+   
+   double temp = current.getDouble("temperature_2m");
+   String tempunit = units.getString("temperature_2m");
+   temp = convertTemperature(temp,tempunit);
+   
+   CatreParameter p0 = findParameter("Temperature");
+   setParameterValue(p0,temp);
+   CatreParameter p1 = findParameter("WeatherCondition");
+   setParameterValue(p1,cond);
+}
+
+
+private double convertTemperature(double v,String unit)
+{
+   return v;
+}
+
+/********************************************************************************/
+/*										*/
+/*	Output methods								*/
+/*										*/
+/********************************************************************************/
+
+@Override public Map<String,Object> toJson()
+{
+   Map<String,Object> rslt = super.toJson();
+
+   rslt.put("VTYPE","OpenMeteo");
+
+   rslt.put("LATITUDE",location_latitude);
+   rslt.put("LONGITUDE",location_longitude);
+   rslt.put("UNITS",unit_type);
+   rslt.put("LOCATION",location_name);
+
+   return rslt;
+}
+
+
+@Override public void fromJson(CatreStore cs,Map<String,Object> map)
+{
+   super.fromJson(cs,map);
+
+   location_latitude = getSavedDouble(map,"LATITUDE",location_latitude);
+   location_longitude = getSavedDouble(map,"LONGITUDE",location_longitude);
+   unit_type = getSavedString(map,"UNITS",unit_type);
+   location_name = getSavedString(map,"LOCATION",location_name);
+}
+
+
+
+
+/********************************************************************************/
+/*										*/
+/*	Program listener							*/
+/*										*/
+/********************************************************************************/
+
+@Override public void programUpdated()
+{
+   // might want to check if this devices is needed by a program
+   is_used = false;
+   Set<CatreParameterRef> refs = getUniverse().getProgram().getActiveSensors();
+   for (CatreParameterRef ref : refs) {
+       CatreDevice cd = ref.getDevice();
+       if (cd != this) continue;
+       is_used = true;
+       break;
+    }
+}
+
+
+/********************************************************************************/
+/*										*/
+/*	Maintain set of active devices						*/
+/*										*/
+/********************************************************************************/
+
+private static synchronized void addMeteoDevice(CatdevMeteoDevice md)
+{
+   if (known_locations == null) {
+      known_locations = new ConcurrentLinkedQueue<>();
+      timer_task = new Updater();
+      md.getCatre().schedule(timer_task,POLL_RATE,POLL_RATE);
+    }
+   known_locations.add(md);
+   updateData(List.of(md));
+  //  updateSingle(md);
+
+}
+
+
+private static void updateData(List<CatdevMeteoDevice> devs)
+{
+   StringBuffer latbuf = new StringBuffer();
+   StringBuffer longbuf = new StringBuffer();
+   for (CatdevMeteoDevice cmd : devs) {
+      if (!latbuf.isEmpty()) latbuf.append(",");
+      latbuf.append(cmd.location_latitude);
+      if (!longbuf.isEmpty()) longbuf.append(",");
+      longbuf.append(cmd.location_longitude);
+    }
+   Map<String,String> map = new HashMap<>();
+   map.put("LAT",latbuf.toString());
+   map.put("LONG",longbuf.toString());
+   String url = IvyFile.expandName(METEO_URL,map);
+   String cnts = WEB_CACHE.getContents(url,0);
+   if (cnts == null) {
+      CatreLog.logE("Problem getting weather data for " + url);
+      return;
+    }
+   if (cnts.startsWith("[")) {
+      JSONArray jarr = new JSONArray(cnts);
+      for (int i = 0; i < jarr.length(); ++i) {
+         devs.get(i).update(jarr.getJSONObject(i));
+       }
+    }
+   else {
+      JSONObject jobj = new JSONObject(cnts);
+      devs.get(0).update(jobj);
+    }
+}
+   
+
+
+
+private static final class Updater extends TimerTask {
+
+   @Override public void run() {
+      List<CatdevMeteoDevice> toupdate = new ArrayList<>();
+      for (CatdevMeteoDevice md : known_locations) {
+	 if (md.isDeviceValid() && md.isEnabled() && md.is_used) {
+	    toupdate.add(md);
+	  }
+       }
+      if (toupdate.isEmpty()) return;
+
+      updateData(toupdate);
+    }
+}
+
+
+
+}	// end of class CatdevMeteoDevice
+
+
+
+
+/* end of CatdevMeteoDevice.java */
+
---
+++ flutter/sherpa/lib/pages/addweatherpage.dart
@@ -0,0 +1,333 @@
+/********************************************************************************/
+/*                                                                              */
+/*              addweatherpage.dart                                             */
+/*                                                                              */
+/*      Page to define a new weather device                                     */
+/*                                                                              */
+/********************************************************************************/
+/*      Copyright 2023 Brown University -- Steven P. Reiss                      */
+/*********************************************************************************
+ *  Copyright 2023, Brown University, Providence, RI.                            *
+ *                                                                               *
+ *                        All Rights Reserved                                    *
+ *                                                                               *
+ *  Permission to use, copy, modify, and distribute this software and its        *
+ *  documentation for any purpose other than its incorporation into a            *
+ *  commercial product is hereby granted without fee, provided that the          *
+ *  above copyright notice appear in all copies and that both that               *
+ *  copyright notice and this permission notice appear in supporting             *
+ *  documentation, and that the name of Brown University not be used in          *
+ *  advertising or publicity pertaining to distribution of the software          *
+ *  without specific, written prior permission.                                  *
+ *                                                                               *
+ *  BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS                *
+ *  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND            *
+ *  FITNESS FOR ANY PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY      *
+ *  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY          *
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,              *
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS               *
+ *  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE          *
+ *  OF THIS SOFTWARE.                                                            *
+ *                                                                               *
+ ********************************************************************************/
+
+import 'package:flutter/material.dart';
+import '../widgets.dart' as widgets;
+import '../util.dart' as util;
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class SherpaAddWeatherPage extends StatefulWidget {
+  const SherpaAddWeatherPage({super.key});
+
+  @override
+  State<SherpaAddWeatherPage> createState() =>
+      _SherpaAddWeatherPageState();
+}
+
+class _SherpaAddWeatherPageState extends State<SherpaAddWeatherPage> {
+  final TextEditingController _locationControl =
+      TextEditingController();
+  final TextEditingController _nameControl = TextEditingController();
+  final TextEditingController _descControl = TextEditingController();
+  final TextEditingController _latControl = TextEditingController();
+  final TextEditingController _longControl = TextEditingController();
+  bool _locChanged = false;
+  bool _nameSet = false;
+  bool _descSet = false;
+  String _units = "Imperial";
+
+  _SherpaAddWeatherPageState();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget w1 = widgets.textField(
+      label: "Location",
+      hint:
+          "Enter location (e.g., city state or city country or postal code)",
+      controller: _locationControl,
+      maxLines: 0,
+      enabled: true,
+      onChanged: (String s) {
+        _locChanged = true;
+      },
+      tooltip:
+          "Enter location name.  Either click elsewhere or click the "
+          "Locate button to convert this to latitude and longitude.  If the location "
+          "might be ambiguous, check the other fields for correctness. "
+          "State postal codes might not work.  Use part of the state name instead",
+    );
+    w1 = Focus(onFocusChange: _locationUpdate, child: w1);
+    Widget w2 = widgets.textField(
+      label: "Latitude",
+      controller: _latControl,
+      enabled: true,
+      readOnly: true,
+      tooltip: "This will be computed from the location",
+    );
+    w2 = Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        const SizedBox(width: 50),
+        Expanded(child: w2),
+      ],
+    );
+    Widget w3 = widgets.textField(
+      label: "Longitude",
+      controller: _longControl,
+      enabled: true,
+      readOnly: true,
+      tooltip: "This will be computed from the location",
+    );
+    w3 = Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        const SizedBox(width: 50),
+        Expanded(child: w3),
+      ],
+    );
+    Widget w4 = widgets.textField(
+      label: "Name",
+      hint: "Enter a name for this weather device",
+      enabled: true,
+      controller: _nameControl,
+      onChanged: (String s) {
+        _nameSet = true;
+      },
+    );
+    Widget w5 = widgets.textField(
+      label: "Description",
+      controller: _descControl,
+      hint: "Enter a detailed description of this device",
+      enabled: true,
+      onChanged: (String s) {
+        _descSet = true;
+      },
+    );
+    Widget w6 = widgets.dropDownWidget<String>(
+      ["Imperial", "Metric"],
+      label: "Units",
+      value: _units,
+      hint: "Choose type of units for weather measurements",
+      onChanged: (String? t) {
+        if (t != null) _units = t;
+      },
+    );
+
+    Widget bottom = Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        widgets.submitButton(
+          "Cancel",
+          _cancelCreate,
+          enabled: true,
+          tooltip: "Go back without creating the device",
+        ),
+        widgets.submitButton(
+          "Locate",
+          _locationUpdate,
+          tooltip: "Recompute the latitude and longitude from location",
+        ),
+        widgets.submitButton(
+          "Create",
+          _createDevice,
+          enabled: _checkCanCreate(),
+          tooltip: "Create this weather device",
+        )
+      ],
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Open-Meteo Weather"),
+      ),
+      body: widgets.topLevelPage(
+        context,
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              w1,
+              widgets.fieldSeparator(),
+              w2,
+              widgets.fieldSeparator(),
+              w3,
+              widgets.fieldSeparator(16),
+              w4,
+              widgets.fieldSeparator(),
+              w5,
+              widgets.fieldSeparator(),
+              w6,
+              widgets.fieldSeparator(16),
+              bottom,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _locationUpdate([
+    bool fg = false,
+  ]) async {
+    if (!fg && _locChanged) {
+      _locChanged = false;
+      String name = _locationControl.text;
+      String? check;
+      int idx = name.indexOf(",");
+      if (idx > 0) {
+        check = name.substring(idx + 1).trim();
+        name = name.substring(0, idx);
+        name = name.trim();
+      }
+      Map<String, dynamic> q = {
+        "name": name,
+        "count": "10",
+        "language": "en",
+        "format": "json",
+      };
+      Uri u = Uri.https("geocoding-api.open-meteo.com", "v1/search", q);
+      dynamic resp = await http.get(u);
+      if (resp.statusCode < 400) {
+        Map<String, dynamic> json0 =
+            convert.jsonDecode(resp.body) as Map<String, dynamic>;
+        List<dynamic>? json1 = json0["results"];
+        if (json1 != null && json1.isNotEmpty) {
+          Map<String, dynamic> json = _findBestMatch(json1, check);
+          _latControl.text = json["latitude"].toString();
+          _longControl.text = json["longitude"].toString();
+          if (!_nameSet) {
+            _nameControl.text = "${_locationControl.text} Weather";
+          }
+          if (!_descSet) {
+            String d = "Weather for ${json['name']}";
+            d += _addOn(json, "admin1");
+            d += _addOn(json, "admin2");
+            // d += _addOn(json, "admin3");
+            // d += _addOn(json, "admin4");
+            d += _addOn(json, "country_code");
+            _descControl.text = d;
+          }
+        }
+      }
+      _locChanged = false;
+      setState(() {});
+    }
+  }
+
+  String _addOn(Map<String, dynamic> json, String key) {
+    String? v = json[key];
+    if (v == null || v.isEmpty) return "";
+    return ", $v";
+  }
+
+  Map<String, dynamic> _findBestMatch(
+      List<dynamic> items, String? check) {
+    if (check == null || check.isEmpty || items.length == 1) {
+      return items.first;
+    }
+    Map<String, dynamic> best = items.first;
+    int bestct = 0;
+    List<String> keys = check.split(",");
+    for (Map<String, dynamic> itm in items) {
+      int ct = _matchCount(itm, keys);
+      if (ct > bestct) {
+        best = itm;
+        bestct = ct;
+      }
+    }
+    return best;
+  }
+
+  int _matchCount(Map<String, dynamic> itm, List<String> keys) {
+    int ct = 0;
+    for (String s in keys) {
+      s = s.toLowerCase().trim();
+      if (_occursIn(itm, s)) ++ct;
+    }
+    return ct;
+  }
+
+  bool _occursIn(Map<String, dynamic> itm, String v) {
+    if (_occursInElt(itm, v, "country_code")) return true;
+    if (_occursInElt(itm, v, "country")) return true;
+    if (_occursInElt(itm, v, "admin1")) return true;
+    if (_occursInElt(itm, v, "admin2")) return true;
+    if (_occursInElt(itm, v, "admin3")) return true;
+    if (_occursInElt(itm, v, "admin4")) return true;
+
+    return false;
+  }
+
+  bool _occursInElt(Map<String, dynamic> itm, String key, String fld) {
+    String? v = itm[fld];
+    if (v == null || v.isEmpty) return false;
+    v = v.toLowerCase().trim();
+    if (v == key) return true;
+    if (v.startsWith(key)) return true;
+    if (v.contains(" $key")) return true;
+    // might want to expand state abbreviations
+    return false;
+  }
+
+  Future<void> _createDevice() async {
+    BuildContext dcontext = context;
+    Map<String, String?> body = {
+      "VTYPE": "OpenMeteo",
+      "UNITS": _units,
+      "LATITUDE": _latControl.text,
+      "LONGITUDE": _longControl.text,
+      "LOCATION": _locationControl.text,
+      "NAME": _nameControl.text,
+      "DESCRIPTION": _descControl.text,
+      "USERDESC": _descSet.toString(),
+    };
+    Map<String, dynmic> rslt =
+        await util.postJson("/universe/addvirtual", body);
+    // might want to check the result
+    if (dcontext.mounted) {
+      Navigator.pop(dcontext, "OK");
+    }
+  }
+
+  void _cancelCreate() {
+    if (context.mounted) {
+      Navigator.pop(context, "CANCEL");
+    }
+  }
+
+  bool _checkCanCreate() {
+    if (_latControl.text != "" &&
+        _longControl.text != "" &&
+        _nameControl.text != "" &&
+        _descControl.text != "") {
+      return true;
+    }
+    return false;
+  }
+}
